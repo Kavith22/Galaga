770d6c0c499e0299e0990a96e13cdf21ae9bdcdf
Kavith22 <kaviththaya@gmail.com>
2025-11-19 18:55:13 +0000

    Add files via upload

--- Galage Game.py
+++ Galage Game.py
@@ -5,32 +5,45 @@ ship=Actor('ship.png')
 ship.x=(250)    
 ship.y=(400)
 
+x=(50)    
+y=(50)
 bullets=[]
+aliens=[]
+
+for i in range(5):
+    alien=Actor('wasp.png')
+    aliens.append(alien)
+    x=x+70
+    aliens[-1].x=x
+    
 def draw():
     screen.clear()
     screen.fill('#414640')
     ship.draw()
     for bullet in bullets:
         bullet.draw()
+    for alien in aliens:
+        alien.draw()
+                                 
 
 def update():
     if keyboard.left:
-        ship.x=ship.x-6
+        ship.x=ship.x-4
     if keyboard.right:
-        ship.x=ship.x+6
+        ship.x=ship.x+4
     if keyboard.space:
         bullet=Actor('bullet.png')
         bullet.x=(ship.x)
         bullet.y=(ship.y-43)
         if len(bullets)<=1:
             bullets.append(bullet)
-        
     for i in bullets:
         i.y=i.y-30
         if i.y<0:
             bullets.remove(i)
-
-        
-
-
-pgzrun.go()
+    for alien in aliens:
+        alien.y=alien.y+0.7 
+        for bullet in bullets:
+            if bullet.colliderect(alien) and len(aliens)>0:
+                aliens.remove(alien)
+pgzrun.go()        
